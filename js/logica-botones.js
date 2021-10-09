1dabd23e65737ffb4d0073e5df9c861f7eabe2ff
AMB <alejo.balboa@yahoo.com.ar>
2021-10-08 23:57:48 -0300

    Agregada primera parte del formulario

--- js/logica-botones.js
+++ js/logica-botones.js
@@ -1,29 +1,36 @@
 /*LOGICA DE LOS BOTONES DEL FORMULARIO*/
 /**************************************/
 
+/*CARGA DEL SITIO*/
+document.getElementById("intro").style.display = "flex";
+document.getElementById("primera_parte").style.display = "none";
+document.getElementById("segunda_parte").style.display = "none";
+/*document.getElementById("tercera_parte").style.display = "none";*/
+
 /*INICIO DEL CUESTIONARIO*/
-document.getElementById("siguiente_1").onclick = function (){
+document.getElementById("empezar_cue").onclick = function (){
         document.getElementById("intro").style.display = "none";
-        document.getElementById("primera_parte").style.display = "block";
+        document.getElementById("primera_parte").style.display = "flex";
     }
 
 /*PRIMERA PAGINA*/
 document.getElementById("anterior_2").onclick = function (){
-        document.getElementById("intro").style.display = "block";
-        //document.getElementById("primera_parte").style.display = "none";
+        document.getElementById("intro").style.display = "flex";
+        document.getElementById("primera_parte").style.display = "none";
     }
 
 document.getElementById("siguiente_2").onclick = function (){
         document.getElementById("primera_parte").style.display = "none";
-        document.getElementById("segunda_parte").style.display = "block";
+        document.getElementById("segunda_parte").style.display = "flex";
     }
 
 /*SEGUNDA PAGINA*/
 document.getElementById("anterior_3").onclick = function (){
-        document.getElementById("primera_parte").style.display = "block";
+        document.getElementById("primera_parte").style.display = "flex";
         document.getElementById("segunda_parte").style.display = "none";
     }
+
 document.getElementById("siguiente_3").onclick = function (){
         document.getElementById("segunda_parte").style.display = "none";
-        document.getElementById("tercera_parte").style.display = "block";
+        document.getElementById("tercera_parte").style.display = "flex";
     }
