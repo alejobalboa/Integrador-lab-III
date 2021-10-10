e177fdbb4ff1b0fee162f90da1aa1572a8566a72
AMB <alejo.balboa@yahoo.com.ar>
2021-10-09 22:57:31 -0300

    Commit dia 9-10-2021

--- js/logica-botones.js
+++ js/logica-botones.js
@@ -2,7 +2,7 @@
 /**************************************/
 
 /*CARGA DEL SITIO*/
-document.getElementById("intro").style.display = "flex";
+document.getElementById("intro").style.display = "block";
 document.getElementById("primera_parte").style.display = "none";
 document.getElementById("segunda_parte").style.display = "none";
 /*document.getElementById("tercera_parte").style.display = "none";*/
@@ -10,27 +10,27 @@ document.getElementById("segunda_parte").style.display = "none";
 /*INICIO DEL CUESTIONARIO*/
 document.getElementById("empezar_cue").onclick = function (){
         document.getElementById("intro").style.display = "none";
-        document.getElementById("primera_parte").style.display = "flex";
+        document.getElementById("primera_parte").style.display = "block";
     }
 
 /*PRIMERA PAGINA*/
 document.getElementById("anterior_2").onclick = function (){
-        document.getElementById("intro").style.display = "flex";
+        document.getElementById("intro").style.display = "block";
         document.getElementById("primera_parte").style.display = "none";
     }
 
 document.getElementById("siguiente_2").onclick = function (){
         document.getElementById("primera_parte").style.display = "none";
-        document.getElementById("segunda_parte").style.display = "flex";
+        document.getElementById("segunda_parte").style.display = "block";
     }
 
 /*SEGUNDA PAGINA*/
 document.getElementById("anterior_3").onclick = function (){
-        document.getElementById("primera_parte").style.display = "flex";
+        document.getElementById("primera_parte").style.display = "block";
         document.getElementById("segunda_parte").style.display = "none";
     }
 
 document.getElementById("siguiente_3").onclick = function (){
         document.getElementById("segunda_parte").style.display = "none";
-        document.getElementById("tercera_parte").style.display = "flex";
+        document.getElementById("tercera_parte").style.display = "block";
     }
