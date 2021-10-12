6c7ac8604464aa6303b594114d09c9ff91bedbc9
AMB <alejo.balboa@yahoo.com.ar>
2021-10-12 00:48:01 -0300

    Validación de finalización de formulario

--- js/logica-botones.js
+++ js/logica-botones.js
@@ -1,3 +1,5 @@
+"use strict"
+
 /*LOGICA DE LOS BOTONES DEL FORMULARIO*/
 /**************************************/
 
@@ -42,8 +44,32 @@ document.getElementById("anterior_4").onclick = function (){
     document.getElementById("tercera_parte").style.display = "none";
 }
 
+
+function validar_checkbox(identificador){
+    var i;
+
+    var valor = document.querySelector('input[name="' + identificador + '"]:checked');   
+
+    if(valor != null){
+        return 1;
+    }
+
+    return 0;
+}
+
+function validar_respuestas() {
+    var resp3 = document.getElementById("preg3").value;
+    var resp6 = document.getElementById("preg6").value;
+    
+    if (!(validar_checkbox("preg1")) || !(validar_checkbox("preg2")) || resp3 == "" || !(validar_checkbox("preg4")) || !(validar_checkbox("preg5")) || resp6 == "" || !(validar_checkbox("preg7"))){
+        console.log("ERROR");
+        return false;
+    }
+    return true;
+}
+
 document.getElementById("finalizar").onclick = function (){
-    if (validacion() = 1){
+    if (validar_respuestas()){
         document.getElementById("primer_titulo").style.display = "none";
         document.getElementById("anterior_2").style.display = "none";
         document.getElementById("siguiente_2").style.display = "none";
@@ -60,3 +86,30 @@ document.getElementById("finalizar").onclick = function (){
         alert("es necesario responder todas las preguntas")
     }
 }    
+
+
+/*FUNCIONES*/
+/*
+function val_resp1(){
+    var i;
+    var preg1 = document.getElementById("preg_1");
+
+    console.log("paso");
+
+    for (i=0; preg1.p1.length; i++){
+        console.log("paso");
+        if (preg1.p1.checked)
+            return 1;
+    }
+
+    return 0;
+}
+
+function validar_respuestas() {
+    
+    if (val_resp1 = 0){
+        alert("ERROR");
+    }
+
+    return true;
+}*/
